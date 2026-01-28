5a25369896aee5a3f78267d84e895e9226f04525
Jesus Magana <maganazero5@gmail.com>
2026-01-27 18:35:04 -0800

    Support sending command

--- tb_movement/src/movement.cpp
+++ tb_movement/src/movement.cpp
@@ -2,9 +2,12 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 
+constexpr int ANGULAR = 0;
+constexpr int LINEAR = 1;
+constexpr float SPEED_OFFSET = 2.0f;
 
 class Movement: public rclcpp::Node
 {
@@ -12,23 +15,21 @@ class Movement: public rclcpp::Node
     Movement()
     : Node("movement"), count_(0)
     {
-        joy_publisher_ = this->create_publisher<std_msgs::msg::String>("/debug/joy_received", 10);
+        cmd_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/cmd_vel", 10);
         joy_subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>(
             "/joy", 10, [this](sensor_msgs::msg::Joy::SharedPtr msg){ receiveFromController(msg); }
         );
     }
     private:
     void receiveFromController(sensor_msgs::msg::Joy::SharedPtr msg) {
-        RCLCPP_INFO(this->get_logger(), "I heard from Axes[0]: '%f'", msg->axes[0]);
-        RCLCPP_INFO(this->get_logger(), "I heard from Axes[1]: '%f'", msg->axes[1]);
+        RCLCPP_INFO(this->get_logger(), "I heard from ANGULAR: '%f'", msg->axes[ANGULAR]);
+        RCLCPP_INFO(this->get_logger(), "I heard from LINEAR: '%f'", msg->axes[LINEAR]);
+        auto message = geometry_msgs::msg::TwistStamped();
+        message.twist.angular.z = msg->axes[ANGULAR] * SPEED_OFFSET;
+        message.twist.linear.x = msg->axes[LINEAR] * SPEED_OFFSET;
+        this->cmd_publisher_->publish(message);
     };
-    void sendHello() {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, " + std::to_string(this->count_++);
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        this->joy_publisher_->publish(message);
-    }
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr joy_publisher_;
+    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr cmd_publisher_;
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subscriber_;
     size_t count_;
 };
