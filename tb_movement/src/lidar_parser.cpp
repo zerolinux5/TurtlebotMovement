b5b3052ba98839f8235c0c80b7205aeec539310c
Jesus Magana <maganazero5@gmail.com>
2026-01-30 17:51:16 -0800

    Switch to single value front and back

--- tb_movement/src/lidar_parser.cpp
+++ tb_movement/src/lidar_parser.cpp
@@ -1,22 +1,15 @@
 #include <memory>
 #include <string>
+#include <algorithm>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "tb_movement_msg/msg/obstacle.hpp"
-#include "tb_movement_msg/msg/obstacle_scan.hpp"
 
 constexpr int ANGULAR = 0;
 constexpr int LINEAR = 1;
 constexpr float SPEED_OFFSET = 2.0f;
-
-struct Ray {
-    float depth;
-    float angle;
-};
-
-constexpr size_t MAX_RAYS = 720;
 
 class LidarParser: public rclcpp::Node
 {
@@ -24,7 +17,7 @@ class LidarParser: public rclcpp::Node
     LidarParser()
     : Node("lidar_parser")
     {
-        scan_publisher_ = this->create_publisher<tb_movement_msg::msg::ObstacleScan>("/obstacles", 10);
+        scan_publisher_ = this->create_publisher<tb_movement_msg::msg::Obstacle>("/obstacles", 10);
         lidar_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
             "/scan", 10, [this](sensor_msgs::msg::LaserScan::SharedPtr msg){ receiveFromLidar(msg); }
         );
@@ -32,34 +25,23 @@ class LidarParser: public rclcpp::Node
     }
     private:
     void receiveFromLidar(sensor_msgs::msg::LaserScan::SharedPtr msg) {
-        RCLCPP_INFO(this->get_logger(), "Lidar data Angles: Min: %f Max: %f Increment: %f", msg->angle_min, msg->angle_max, msg->angle_increment);
-        RCLCPP_INFO(this->get_logger(), "Lidar data: Time increment: %f Scan Time %f", msg->time_increment, msg->scan_time);
-        RCLCPP_INFO(this->get_logger(), "Lidar data: Range: Min: %f Max: %f", msg->range_min, msg->range_max);
-        RCLCPP_INFO(this->get_logger(), "Lidar data: First: %f Last: %f", msg->ranges[134], msg->ranges[226]);
-        RCLCPP_INFO(this->get_logger(), "Lidar data: Ranges Size: %ld", msg->ranges.size());
-        std::array<Ray, MAX_RAYS> buf;
         size_t n = 92;
-        for (size_t idx = 0; idx < n; ++idx) {
-            size_t idx_with_offset = start_idx_ + idx;
-            buf[idx] = Ray{msg->ranges[idx_with_offset], msg->angle_increment * idx + msg->angle_min};
+        size_t offset = 360;
+        tb_movement_msg::msg::Obstacle scan_msg;
+        float front_min = msg->ranges[start_idx_];
+        float back_min = msg->ranges[start_idx_ + offset];
+        for (size_t idx = 1; idx < n; ++idx) {
+            size_t front_with_offset = start_idx_ + idx;
+            size_t back_with_offset = start_idx_ + idx + offset;
+            front_min = std::min<float>(front_min, msg->ranges[front_with_offset]);
+            back_min = std::min<float>(back_min, msg->ranges[back_with_offset]);
         }
-        std::sort(buf.begin(), buf.begin() + n, [](const Ray& a, const Ray& b){ return a.depth < b.depth; });
-        for (size_t idx = 0; idx < n; ++idx) {
-            auto& ray = buf[idx];
-            RCLCPP_INFO(this->get_logger(), "- Distance: %f Angle: %f", ray.depth, ray.angle);
-        }
-        tb_movement_msg::msg::ObstacleScan scan_msg;
-        scan_msg.obstacles.reserve(n);
-
-        for (size_t idx = 0; idx < n; ++idx) {
-            tb_movement_msg::msg::Obstacle o;
-            o.depth_m = buf[idx].depth;
-            o.angle_rad = buf[idx].angle;
-            scan_msg.obstacles.push_back(o);
-        }
+        RCLCPP_INFO(this->get_logger(), "Publishing Front Min: %f Back Min: %f", front_min, back_min);
+        scan_msg.closest_front_m = front_min;
+        scan_msg.closest_back_m = back_min;
         scan_publisher_->publish(scan_msg);
     };
-    rclcpp::Publisher<tb_movement_msg::msg::ObstacleScan>::SharedPtr scan_publisher_;
+    rclcpp::Publisher<tb_movement_msg::msg::Obstacle>::SharedPtr scan_publisher_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_subscriber_;
     size_t start_idx_;
 };
--- tb_movement/src/local_planner.cpp
+++ tb_movement/src/local_planner.cpp
@@ -3,7 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
-#include "tb_movement_msg/msg/obstacle_scan.hpp"
+#include "tb_movement_msg/msg/obstacle.hpp"
 
 constexpr float MIN_DIST = 0.5f;
 
@@ -11,33 +11,37 @@ class LocalPlanner: public rclcpp::Node
 {
     public:
     LocalPlanner()
-    : Node("local_planner"), is_front_clear_(false)
+    : Node("local_planner"), is_front_clear_(false), is_back_clear_(false)
     {
         cmd_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/cmd_vel", 10);
-        obstacles_subscriber_ = this->create_subscription<tb_movement_msg::msg::ObstacleScan>(
-            "/obstacles", 10, [this](tb_movement_msg::msg::ObstacleScan::SharedPtr msg){ receiveFromObstacles(msg); }
+        obstacles_subscriber_ = this->create_subscription<tb_movement_msg::msg::Obstacle>(
+            "/obstacles", 10, [this](tb_movement_msg::msg::Obstacle::SharedPtr msg){ receiveFromObstacles(msg); }
         );
         global_direction_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
             "/raw_reading", 10, [this](geometry_msgs::msg::TwistStamped::SharedPtr msg){ receiveFromGlobal(msg); }
         );
     }
     private:
-    void receiveFromObstacles(tb_movement_msg::msg::ObstacleScan::SharedPtr msg) {
-        bool new_reading = true;
-        for (const auto& obstacle: msg->obstacles) {
-            if (obstacle.depth_m < MIN_DIST) {
-                new_reading = false;
-                break;
-            }
+    void receiveFromObstacles(tb_movement_msg::msg::Obstacle::SharedPtr msg) {
+        bool is_new_front_clear = true;
+        if (msg->closest_front_m < MIN_DIST) {
+            is_new_front_clear = false; 
         }
-        is_front_clear_ = new_reading;
+        is_front_clear_ = is_new_front_clear;
+        bool is_new_back_clear = true;
+        if (msg->closest_back_m < MIN_DIST) {
+            is_new_back_clear = false; 
+        }
+        is_back_clear_ = is_new_back_clear;
     };
 
     void receiveFromGlobal(geometry_msgs::msg::TwistStamped::SharedPtr msg) {
         auto message = geometry_msgs::msg::TwistStamped();
         message.twist.angular.z = msg->twist.angular.z;
         // Only move forward if front is clear
-        if (is_front_clear_) {
+        if (msg->twist.linear.x > 0.0f && is_front_clear_) {
+            message.twist.linear.x = msg->twist.linear.x;
+        } else if (msg->twist.linear.x < 0.0f && is_back_clear_) {
             message.twist.linear.x = msg->twist.linear.x;
         }
         this->cmd_publisher_->publish(message);
@@ -45,8 +49,9 @@ class LocalPlanner: public rclcpp::Node
 
     rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr cmd_publisher_;
     rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr global_direction_;
-    rclcpp::Subscription<tb_movement_msg::msg::ObstacleScan>::SharedPtr obstacles_subscriber_;
+    rclcpp::Subscription<tb_movement_msg::msg::Obstacle>::SharedPtr obstacles_subscriber_;
     bool is_front_clear_;
+    bool is_back_clear_;
 };
 
 int main(int argc, char *argv[])
